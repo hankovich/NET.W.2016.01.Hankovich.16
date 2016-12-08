3dfc9251de7e7203d00061b4e174f5c67bf85dbd
hankovich <svyatoslav.hankovich@gmail.com>
2016-12-08 21:11:02 +0300

    Add ability of adding new selling point

--- Task1.Logic/Class1.cs
+++ Task1.Logic/Class1.cs
@@ -16,7 +16,7 @@ namespace Task1.Logic
             +Продажа шаурмы (уменьшение количества продуктов на складе).
             +Создание нового рецепта шаурмы.
             +Установка новой цены.
-            Добавление новой торговой точки.
+            +Добавление новой торговой точки.
             Добавление нового продавца-повара.
             Составление отчета по продажам конкретной торговой точки за указанный  временной интервал.
             Составление отчета по каждому продавцу, заработная плата которого складывается из суммарного времени нахождения в торговой точке и времени, потраченного на приготовление шаурмы, за указанный  временной интервал.
@@ -150,6 +150,26 @@ namespace Task1.Logic
                     pc.Comment = comment;
                 }
 
+                return SaveIt(sb);
+            }
+        }
+
+        public static bool AddSellingPoint(string title, string address, string category)
+        {
+            using (var sb = new ShawarmaBaseEntities())
+            {
+                var pointCat = sb.SellingPointCategory.FirstOrDefault(selling => selling.SellingPointCategoryName == category);
+                if (pointCat == null)
+                    return false;
+
+                SellingPoint point = new SellingPoint
+                {
+                    ShawarmaTitle = title,
+                    Address = address,
+                    SellingPointCategoryId = pointCat.SellingPointCategoryId
+                };
+
+                sb.SellingPoint.Add(point);
                 return SaveIt(sb);
             }
         }
